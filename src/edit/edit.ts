bc0c392868b9342dc15619f9e0754cf6c8085cc0
R2VvcmdlUk5H <81434111+GeorgeRNG@users.noreply.github.com>
2022-03-19 18:10:39 +0000

    feat: :sparkles: Rename Variables
    Set the name of all variables of a scope and name

--- src/edit/edit.ts
+++ src/edit/edit.ts
@@ -1,6 +1,6 @@
 import { startup, decode, menu, minecraftColorHTML, dfNumber, snackbar, codeutilities, cuopen, encode, user } from "../main/main";
 import { ActionDump, CodeBlockIdentifier, CodeBlockTypeName } from "./actiondump";
-import { Template, Block, SelectionBlock, SubActionBlock, BlockTag, DataBlock, SelectionBlocks, SelectionValues, Target, PlacedBlock, Bracket, BracketType } from "./template";
+import { Template, Block, SelectionBlock, SubActionBlock, BlockTag, DataBlock, SelectionBlocks, SelectionValues, Target, Bracket, BracketType, VarScope} from "./template";
 
 let ActDB : ActionDump
 fetch('https://webbot.georgerng.repl.co/db') // Gets ?actiondump.
@@ -89,10 +89,14 @@ window.onload = () => { // when everything loads - this function is pretty hard 
 	}
 	contextMenu.oncontextmenu = e => {(e.target as HTMLElement).click(); e.preventDefault();};
 
-	document.getElementById('file').onclick = e => {
+	menuBar();
+}
+
+function menuBar(){
+	document.querySelector<HTMLButtonElement>('button#file').onclick = e => {
 		e.stopPropagation();
-		var {top, bottom} = (e.target as HTMLButtonElement).getBoundingClientRect();
-		contextMenu.style.left = String(top) + "px";
+		var {left, bottom} = (e.target as HTMLButtonElement).getBoundingClientRect();
+		contextMenu.style.left = String(left) + "px";
 		contextMenu.style.top = String(bottom) + "px";
 		contextMenu.innerHTML = '';
 		contextMenu.style.display = 'grid';
@@ -154,6 +158,74 @@ window.onload = () => { // when everything loads - this function is pretty hard 
 			menu('Export',exportDiv);
 		}
 		contextMenu.append(exportTemplateButton);
+	}
+	document.querySelector<HTMLButtonElement>('button#edit').onclick = e => {
+		console.log(e.target);
+		e.stopPropagation();
+		var {left, bottom} = (e.target as HTMLButtonElement).getBoundingClientRect();
+		contextMenu.style.left = String(left) + "px";
+		contextMenu.style.top = String(bottom) + "px";
+		contextMenu.innerHTML = '';
+		contextMenu.style.display = 'grid';
+
+		var renameVars = document.createElement('button');
+		renameVars.innerText = 'Rename All Variables';
+		renameVars.onclick = () => {
+			var menuDiv = document.createElement('div');
+			menuDiv.style.display = 'grid';
+
+			var lookValue = document.createElement('input');
+			lookValue.placeholder = 'Variable Name';
+			menuDiv.append(lookValue);
+
+			var replaceValue = document.createElement('input');
+			replaceValue.placeholder = 'New Name'
+			menuDiv.append(replaceValue);
+
+			var scopeValue = document.createElement('select');
+			var opt = document.createElement('option');
+			opt.value = 'unsaved';
+			opt.text = 'GAME';
+			scopeValue.append(opt);
+			opt = document.createElement('option');
+			opt.value = 'save';
+			opt.text = 'SAVE';
+			scopeValue.append(opt);
+			opt = document.createElement('option');
+			opt.value = 'local';
+			opt.text = 'LOCAL';
+			scopeValue.append(opt);
+			menuDiv.append(scopeValue);
+			menuDiv.append(document.createElement('br'));
+
+
+			{
+				var replace = document.createElement('button');
+				replace.innerText = 'Rename All'
+				replace.onclick = () => {
+					let results = 0;
+					code.blocks = code.blocks.map(e => {
+						var newBlock = e;
+						if((e as SubActionBlock).args !== undefined){
+							(e as SelectionBlock).args.items = (e as SelectionBlock).args.items.map(x => {
+								var newItem = x;
+								if(newItem.item.id === 'var' && newItem.item.data.scope === (scopeValue.value as VarScope) && newItem.item.data.name === lookValue.value){
+									newItem.item.data.name = replaceValue.value;
+									results++;
+								}
+								return newItem
+							})
+						}
+						return newBlock;
+					})
+					snackbar(`Found ${results} variables.`)
+				}
+				menuDiv.append(replace);
+			}
+			menu('Rename All Variables',menuDiv);
+
+		}
+		contextMenu.append(renameVars);
 	}
 }
 
