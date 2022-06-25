b171a568d0e7901255020638ce5a609130438e58
R2VvcmdlUk5H <81434111+GeorgeRNG@users.noreply.github.com>
2022-06-25 11:38:33 +0100

    Clean up

--- src/main/context/index.ts
+++ src/main/context/index.ts
@@ -2,18 +2,21 @@ import ContextMenu from "../context";
 
 window.onload = function() {
     ContextMenu.setup();
-
-    const funny = document.createElement('button')
-    funny.innerText = 'funni'
-
-    const xd = document.createElement('button');
-    xd.innerText = 'xd'
-
-    const thatContextMenu = new ContextMenu('Yes!',[funny,xd,subMenu.subMenu],true)
     const that = document.getElementById('that');
     that.onclick = e => thatContextMenu.toggle(e)
 }
 
 const input = document.createElement('input');
 input.placeholder = 'yes';
+input.onkeydown = e => {if(e.key === 'Enter'){thatContextMenu.close(); subMenu.close()}}
 const subMenu = new ContextMenu('uhuh',[input],true)
+
+const funny = document.createElement('button')
+funny.innerText = 'funni'
+funny.onclick = () => {thatContextMenu.close()};
+
+const xd = document.createElement('button');
+xd.innerText = 'xd'
+xd.onclick = () => {thatContextMenu.close()};
+
+const thatContextMenu = new ContextMenu('Yes!',[funny,xd,subMenu.subMenu],true)
