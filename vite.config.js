184551aa2a57dc4f19a77c32672e3d33b63a2562
R2VvcmdlUk5H <81434111+GeorgeRNG@users.noreply.github.com>
2022-06-28 17:42:26 +0100

    set up build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
+// @ts-check
+const root = './src';
+
+import { resolve } from 'path';
+
 /**
  * @type {import('vite').UserConfig}
  */
 export default 
 {
     base: './',
-    root: './src',
+    root,
     server: {
-        port: 1234
+        port: 1234,
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(root,'index.html'),
+                edit: resolve(root,'edit/index.html'),
+                comment: resolve(root,'edit/comment/index.html'),
+                how: resolve(root,'edit/how/index.html'),
+                home: resolve(root,'home/index.html'),
+            }
+        },
+        outDir: '../dist',
     }
 }
+
