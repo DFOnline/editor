03eea83017fa09f148c73a9dd26cf3deb4c30f4c
R2VvcmdlUk5H <81434111+GeorgeRNG@users.noreply.github.com>
2022-08-18 19:03:23 +0100

    ✅ test page this is probably the incorrect use of the test gitmoji

---
+++ src/test/selection/index.ts
@@ -0,0 +1,15 @@
+import ActionDump, { CodeBlockTypeName } from "../../edit/ts/actiondump";
+import ContextMenu from "../../main/context";
+import SelectionContext from "../../main/SelectionContext";
+
+ContextMenu.setup();
+
+const button = document.querySelector('button');
+
+button.onclick = async e => {
+    const type = CodeBlockTypeName.player_action;
+    const values = Object.fromEntries((await ActionDump).actions.filter(a => a.codeblockName === type).map(a => [a.name,[...a.aliases,a.name]]));
+    const ctx = new SelectionContext('Actions',values,false);
+    ctx.toggle(e);
+    ctx.callback = console.log;
+}
