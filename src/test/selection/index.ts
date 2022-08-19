373a68c0421a4b4cdeb707df4c910ecc9acade25
R2VvcmdlUk5H <81434111+GeorgeRNG@users.noreply.github.com>
2022-08-19 10:45:44 +0100

    ✅ Update test page

--- src/test/selection/index.ts
+++ src/test/selection/index.ts
@@ -9,7 +9,7 @@ const button = document.querySelector('button');
 button.onclick = async e => {
     const type = CodeBlockTypeName.player_action;
     const values = Object.fromEntries((await ActionDump).actions.filter(a => a.codeblockName === type).map(a => [a.name,[...a.aliases,a.name]]));
-    const ctx = new SelectionContext('Actions',values,false);
+    const ctx = new SelectionContext('Actions',values,true,false);
     ctx.toggle(e);
     ctx.callback = console.log;
 }
