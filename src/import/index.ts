8832b01f822df41cbba716fe4c25f338c92a5a32
R2VvcmdlUk5H <81434111+GeorgeRNG@users.noreply.github.com>
2022-08-31 18:58:50 +0100

    💄 Make it look nice

--- src/import/index.ts
+++ src/import/index.ts
@@ -1,12 +1,23 @@
+import { snackbar } from "../main/main";
 import { book } from "./import";
 
 const pages = document.querySelector<HTMLDivElement>('div#pages');
 const input = document.querySelector<HTMLInputElement>('input#input');
+const title = document.querySelector<HTMLHeadingElement>('h2#title');
+
 input.onchange = () => {
     pages.innerHTML = '';
-    book(input.value).forEach(p => {
+    const pagearray = book(input.value);
+    title.innerText = pagearray.length + ' pages';
+    pagearray.forEach((p,i) => {
         const page = document.createElement('p');
         page.innerText = p;
+        page.title = 'Page ' + (i + 1)
+        page.onclick = () => {
+            navigator.clipboard.writeText(p);
+            snackbar(`Coped page ${i + 1} to clipboard`);
+            page.classList.toggle('used');
+        }
         pages.append(page);
     })
 }
