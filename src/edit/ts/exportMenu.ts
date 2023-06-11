b0d5d27b5e0e9275c1f4ddf9109086a4b5b80bbf
GeorgeRNG <grogrng@gmail.com>
2023-06-11 10:12:30 +0100

    ✨ copy give command

--- src/edit/ts/exportMenu.ts
+++ src/edit/ts/exportMenu.ts
@@ -17,8 +17,8 @@ copyTemplate.innerText = "Copy Data";
 copyTemplate.onclick = e => {
     let data = exportTemplate(JSON.stringify(code));
     let altName = data.name.replace('"', '\\"').replace('\\', '\\\\').replace("'", "\\'");
-    if (e.shiftKey || e.ctrlKey) navigator.clipboard.writeText(`/dfgive minecraft:ender_chest{display:{Name:'{"text":"${altName}"}'},PublicBukkitValues:{"hypercube:codetemplatedata":'{name:"${altName}",code:"${data.data}",version:1,author:"${data.author}"}'}} 1`);
-    else navigator.clipboard.writeText(data.data);
+    if (e.shiftKey || e.ctrlKey) navigator.clipboard.writeText(`/dfgive minecraft:ender_chest{display:{Name:'{"text":"${altName}"}'},PublicBukkitValues:{"hypercube:codetemplatedata":'{name:"${altName}",code:"${data.code}",version:1,author:"${data.author}"}'}} 1`);
+    else navigator.clipboard.writeText(data.code);
 }
 options.append(copyTemplate);
 
@@ -39,14 +39,21 @@ CodeUtilsSend.onclick = () => { // the code for sending :D
 }
 options.append(CodeUtilsSend);
 
-
+const CopyGiveCommandButton = document.createElement('button');
+CopyGiveCommandButton.innerText = 'Copy Give Command';
+CopyGiveCommandButton.onclick = () => {
+    navigator.clipboard.writeText(`/give @p ender_chest{display:{Name:"\\"DFOnline Template\\""},PublicBukkitValues:{"hypercube:codetemplatedata":'${JSON.stringify(
+        exportTemplate(JSON.stringify(code))
+    )}'}}`)
+}
+options.append(CopyGiveCommandButton)
 
 const CopyLinkButton = document.createElement('button');
 CopyLinkButton.innerText = 'Copy Link';
 CopyLinkButton.onclick = async () => { // this code is for copying the link to the template, so you can share the template with others.
     let href = 'https://dfonline.dev/edit/';
     let searchParams = new URLSearchParams(location.search);
-    let exportData = exportTemplate(JSON.stringify(code)).data;
+    let exportData = exportTemplate(JSON.stringify(code)).code;
     searchParams.set('template', exportData);
     navigator.clipboard.writeText(href + '?' + searchParams.toString());
 }
@@ -57,7 +64,7 @@ CopyShortLinkButton.innerText = 'Copy Short Link';
 CopyShortLinkButton.onclick = async e => {
     const href = !e.shiftKey ? 'https://dfonline.dev/edit/' : 'https://diamondfire.gitlab.io/template/';
     let searchParams = new URLSearchParams(location.search);
-    let exportData: string = (await fetch(`${window.sessionStorage.getItem('apiEndpoint')}save`, { body: exportTemplate(JSON.stringify(code)).data, method: 'POST' }).then(res => res.json())).id;
+    let exportData: string = (await fetch(`${window.sessionStorage.getItem('apiEndpoint')}save`, { body: exportTemplate(JSON.stringify(code)).code, method: 'POST' }).then(res => res.json())).id;
     searchParams.set(e.shiftKey ? 't' : 'template', e.shiftKey ? 'dfo:' + exportData : exportData);
     navigator.clipboard.writeText(href + '?' + searchParams.toString());
 }
